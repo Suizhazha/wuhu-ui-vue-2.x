a8eef5e8d3beffc7b33ee03ecfabd2ee634da15a
Suizhazha <573505403@qq.com>
2020-08-25 21:46:20 +0800

    更新了icon的样式

---
+++ src/icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg class="wuhu-icon">
+
+    <!-- ``中是JS字符串，${icon}是模板字符串的插值-->
+    <use :xlink:href="`#i${name}`"></use>
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'icon.vue',
+  props:['name']
+};
+</script>
+
+<style lang="scss" scoped>
+.wuhu-icon{
+  width: 1em;
+  height: 1em;
+}
+</style>
