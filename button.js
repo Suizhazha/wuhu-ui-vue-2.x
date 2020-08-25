80ca53ac1c86fcbfcd4e28b95c37197c4a798dc2
Suizhazha <573505403@qq.com>
2020-08-25 16:43:13 +0800

    修改为单文件组件模式

--- button.js
+++
@@ -1,7 +0,0 @@
-Vue.component('wuhu-button',{
-   template:`
-   <div>
-   <button class="wuhu-button">提交</button>
-</div>
-   `,
-})
---
+++ src/button.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+<button class="wuhu-button">提交</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'button.vue',
+
+};
+</script>
+
+<style lang="scss" scoped>
+.wuhu-button {
+  font-size:var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover{
+    border-color:var(--button-active-bg);
+  }
+
+  &:active {
+    background-color:var(--border-color-hover);
+  }
+  &:focus{
+    outline: none ;
+  }
+}
+</style>
