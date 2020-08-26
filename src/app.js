e1da9b3e6e6bf01446ed9e15bdbf709b338c0ea8
Suizhazha <573505403@qq.com>
2020-08-26 14:56:31 +0800

    创建了button-group

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
-import Button from './button'
-import Icon from './icon'
-
+import Button from './button.vue'
+import Icon from './icon.vue'
+import ButtonGroup from './button-group.vue'
 
 Vue.component('wuhu-button',Button)
 Vue.component('wuhu-icon',Icon)
+Vue.component(' wuhu-button-group',ButtonGroup)
 
 new Vue({
   el:'#app',
